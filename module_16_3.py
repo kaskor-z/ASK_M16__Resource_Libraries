00187d4dfdfbc86a051dfa9c28f0fa20715a36c1
kaskor-z <kaskor.z@yandex.ru>
2024-11-04 13:47:09 +0700

    Update module_16_3.py

--- module_16_3.py
+++ module_16_3.py
@@ -6,20 +6,10 @@ app = FastAPI()
 
 users = {'1': 'Имя: Example, возраст: 18'}
 
-"""
-get запрос по маршруту '/users', 
-    который возвращает словарь users.
-"""
 @app.get("/")
 async def get_all_users() -> dict:
     return users
 
-"""
-post запрос по маршруту '/user/{username}/{age}', 
-    который добавляет в словарь по максимальному значению ключа строки 
-    "Имя: {username}, возраст: {age}". 
-    И возвращает строку "User <user_id> is registered".
-"""
 @app.post("/user/{username}/{age}")
 async def create_user(username: Annotated[str, Path(min_length=5, max_length=20,
                                                      description="Enter username", example="little_pony")],
@@ -28,12 +18,6 @@ async def create_user(username: Annotated[str, Path(min_length=5, max_length=20,
     users[current_index] = f"Имя: {username}, возраст: {age}"
     return f"User № {current_index} is registered"
 
-"""
-put запрос по маршруту '/user/{user_id}/{username}/{age}', 
-    который обновляет значение из словаря users 
-    под ключом user_id на строку "Имя: {username}, возраст: {age}". 
-    И возвращает строку "The user <user_id> is registered"
-"""
 @app.put("/user/{user_id}/{username}/{age}")
 async def update_user(user_id: Annotated[int, Path(ge=1, le=100, description="Enter User ID", example="1")],
                       username: str = Path(min_length=5, max_length=20,
@@ -42,10 +26,6 @@ async def update_user(user_id: Annotated[int, Path(ge=1, le=100, description="En
     users[user_id] = f"Имя: {username}, возраст: {age}"
     return f"The user № {user_id} has been updated"
 
-"""
-delete запрос по маршруту '/user/{user_id}', 
-    который удаляет из словаря users по ключу user_id пару.
-"""
 @app.delete("/user/{user_id}")
 async def delete_user(user_id: int = Path(ge=1, le=100, description="Enter User ID", example="1")) -> str:
     if users.get(str(user_id)) == None:
