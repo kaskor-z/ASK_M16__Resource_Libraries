4c4fbe140426da8e7174a4b6adeb13f1b7ef80cd
kaskor-z <kaskor.z@yandex.ru>
2024-11-10 15:58:26 +0700

    Add files via upload

--- module_16_5.py
+++ module_16_5.py
@@ -15,16 +15,28 @@ class User(BaseModel):
     username: str
     age: int
 
+
+class UserNotFound(Exception):
+    def __init__(self, err_message, add_info):
+        self.err_message = err_message
+        self.add_info = add_info
+
+
 @app.get("/")
 async def get_all_users(request: Request) -> HTMLResponse:
     return templates.TemplateResponse("users.html", {"request": request, "users": users})
 
 @app.get(path="/user/{user_id}")
-async def get_all_users(request: Request, user_id: int) -> HTMLResponse:
+async def get_user(request: Request, user_id: int) -> HTMLResponse:
     try:
-        return templates.TemplateResponse("users.html", {"request": request, "user": users[user_id-1]})
-    except IndexError:
-        return HTTPException(status_code=404, detail="User not found")
+        user = next(filter(lambda user: user.id == user_id, users), None)
+        if user == None:
+            raise UserNotFound("*** ERROR GET *** Oops! User can't will be getting from DB. User not found",
+                               f" --> add information: user_id = {user_id} --> list users size = {len(users)}")
+        return templates.TemplateResponse("users.html", {"request": request, "user": user})
+    except UserNotFound as UNF_:
+        print(f"ERROR: status cod = 410  {UNF_.err_message}")
+        return HTTPException(status_code=410, detail=UNF_.err_message + UNF_.add_info)
 
 @app.post("/user/{username}/{age}")
 async def create_user(user: User, username: Annotated[str, Path(min_length=5, max_length=20,
@@ -32,12 +44,15 @@ async def create_user(user: User, username: Annotated[str, Path(min_length=5, ma
                                                                 example="User_0")],
                                         age: int = Path(ge=18, le=120, description="Enter age",
                                                         example="24")) -> str:
-     if users:
+    if users:
          user_id = max(users, key=lambda m: m.id).id + 1
-     else:
+    else:
          user_id = 1
-     users.append(User(id=user_id, username=username, age=age))
-     return  f"User № {user_id} is registered"
+    user.id = user_id
+    user.username = username
+    user.age = age
+    users.append(user)
+    return f"User {username} № {user_id} is registered"
 
 @app.put("/user/{user_id}/{username}/{age}")
 async def update_user(user_id: Annotated[int, Path(ge=0, le=100,
@@ -47,18 +62,25 @@ async def update_user(user_id: Annotated[int, Path(ge=0, le=100,
                       age: int = Path(ge=18, le=120, description="Enter age", example="24")) -> str:
     try:
         edit_user = next(filter(lambda user: user.id == user_id, users), None)
+        if edit_user == None:
+            raise UserNotFound("*** ERROR UPDATE *** Oops! User can't be updated to DB. User not found",
+                               f" --> add information: user_id = {user_id} --> list users size = {len(users)}")
         edit_user.username = username
         edit_user.age = age
         return f"User {edit_user} updated!"
-    except IndexError:
-        raise HTTPException(status_code=404, detail="User not found")
+    except UserNotFound as UNF_:
+        print(f"ERROR: status cod = 410  {UNF_.err_message}")
+        raise HTTPException(status_code=410, detail=UNF_.err_message + UNF_.add_info)
 
 @app.delete("/user/{user_id}")
 async def delete_user(user_id: int = Path(ge=0, le=100, description="Enter User ID", example="1")) -> str:
     try:
         edit_user = next(filter(lambda user: user.id == user_id, users), None)
+        if edit_user == None:
+            raise UserNotFound("*** ERROR DELETE *** Oops! User can't be removed from DB. User not found",
+                               f" --> add information: user_id = {user_id} --> list users size = {len(users)}")
         users.remove(edit_user)
         return f"User with number {user_id} has been deleted from Users DB"
-    except ValueError:
-        raise HTTPException(status_code=500, detail="Oops! User not found")
-
+    except UserNotFound as UNF_:
+        print(f"ERROR: status cod = 410  {UNF_.err_message}")
+        raise HTTPException(status_code=410, detail=UNF_.err_message + UNF_.add_info)
